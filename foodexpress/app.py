4da5af530d96187f11685cf122c033c44bec1b78
JoaoVictor-99 <joaosuemia@gmail.com>
2026-05-20 17:01:17 -0300

    Add files via upload

--- foodexpress/app.py
+++ foodexpress/app.py
@@ -1,10 +1,56 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return 'Olá Mundo!'
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/cardapio")
+def cardapio():
+    return render_template("cardápio.html")
+
+@app.route("/lanche/<nome>")
+def lanche(nome):
+    mensagens = {
+        "pizza": "Pizza quentinha e deliciosa!",
+        "hamburguer": "Hambúrguer suculento e saboroso!",
+        "batata": "Batata frita crocante!",
+        "milkshake": "Milkshake gelado e cremoso!"
+    }
+
+    if nome.lower() in mensagens:
+        mensagem = mensagens[nome.lower()]
+    else:
+        mensagem = "Lanche não encontrado."
+
+    return render_template(
+        "lanche.html",
+        nome=nome,
+        mensagem=mensagem
+    )
+
+@app.route("/pedidos")
+def pedidos():
+    return render_template("pedidos.html")
+
+@app.route("/cliente/<nome>/<cidade>")
+def cliente(nome, cidade):
+    if cidade.lower() == "natal":
+        entrega = "Entrega disponível"
+    else:
+        entrega = "Entrega indisponível."
+
+    return render_template(
+        "cliente.html",
+        nome=nome,
+        cidade=cidade,
+        entrega=entrega
+    )
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
